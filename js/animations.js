4b99f666e4803a98ae10685ae7b2e53079a6b36c
Ksandr <avk-freelancer@rambler.ru>
2022-12-04 16:52:26 +0300

    Обновили форму с выбором бюджета с помощью JS

--- js/animations.js
+++ js/animations.js
@@ -22,3 +22,28 @@ function initIncreaseNumberAnimation() {
 }
 
 initIncreaseNumberAnimation();
+
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    // Добавляем еще одно текстовое поле
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input');
+
+    const input = document.createElement('input');
+    input.placeholder = 'Введите ваш вариант';
+    input.type = 'text';
+
+    formContainer.appendChild(input);
+    // Вставсляем текстовое поле перед кнопкой
+    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+  }
+
+  const otherInput = document.querySelector('.form__other-input');
+
+  if (event.target.value !== 'other' && otherInput) {
+    // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+    document.querySelector('#form form').removeChild(otherInput);
+  }
+});
